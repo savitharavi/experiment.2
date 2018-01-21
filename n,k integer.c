89c767ca4dcc20e26395824e3781479a3649d575
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-01-21 11:13:53 +0530

    Create n,k integer.c

---
+++ n,k integer.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int array[25],limt,a,b;
+printf("enter the array"\n):
+scanf("%d",&limit);
+printf("enter the"%d" element",limit);
+for(a=0;a<limit;a++)
+{
+scanf("%d",array[a]);
+}
+printf("The element is\n");
+{
+for(b=0;b<limit;b++)
+{
+if(array[a]=array[b])
+break;
+}
+if(a==b)
+{
+printf("%d",array[a]);
+}
+}
+getch()
+return 0
+}
