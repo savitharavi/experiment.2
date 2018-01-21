6f546e835847fedefb4cb8f5c0cec51018b226a7
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-01-21 11:55:53 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,t,rem,result=0;
+printf("Enter the no");
+scanf("%d",&n);
+t=n;
+while(n!=0)
+{
+rem=n%10;
+result=result+rem*rem*rem;
+n=n/10;
+}
+if(t==result)
+{
+printf("%d is an armstrong no",n);
+}
+else
+{
+printf("%d is not an armstrong",n);
+}
+return 0;
+}
