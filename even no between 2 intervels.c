b3fd3b9ce9dceaf32e3250d9dc9f829785d08cb8
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-01-28 15:47:00 +0530

    Create even no between 2 intervels.c

---
+++ even no between 2 intervels.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int count,n;
+printf("the even no between 1to 50 intervels\n");
+for(count=1;count<=50;count++)
+{
+if(count%2==0)
+{
+printf("%d\n",count);
+}
+}
+return 0;
+}
