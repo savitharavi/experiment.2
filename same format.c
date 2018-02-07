7bec6c38d2c42c99e2373c356e03612b8d27735a
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-02-07 16:09:39 +0530

    Create same format.c

---
+++ same format.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ #include<conio.h>
+int main()
+{
+	int i,j,l,m,n,k,o;
+	scanf("%d%d%d%d",&i,&j,&k,&l);
+	printf("INPUT\n%d %d\n%d %d",i,j,k,l);
+	n=i-l;
+	o=j-m;
+	printf("\nOUTPUT\n%d %d",n,o);
+	return 0;
+}
