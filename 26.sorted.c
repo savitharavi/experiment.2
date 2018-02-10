105b71c39e320831b496eab018d97eb7d4f59e3e
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-02-10 13:19:33 +0530

    Create 26.sorted.c

---
+++ 26.sorted.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+int array[10];
+int size;
+int i,j,t;
+printf("Enter the size of array:");
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+for(j=0;j<size;j++)
+{
+if(array[i]>array[j])
+{
+t=array[i];
+array[i]=array[j];
+array[j]=t;
+}
+}
+}
+for(i=0;i<size;i++)
+{
+printf("%d\t",array[i]);
+}
+return 0;
+}
