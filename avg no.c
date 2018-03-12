3df75d6d676c6fc027c0f69fc1d3a7142a26b5cf
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-03-12 16:49:43 +0530

    Create avg no.c

---
+++ avg no.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b[10],avg,i,add;
+printf("enter the count:");
+scanf("%d",&a);
+printf("enter the numbers:");
+ for(i=0;i<a;i++)
+ {
+     scanf("%d",&b[10]);
+ }
+ add=add+b[10];
+ printf("the entered no's are:%d\n",a);
+ avg=b[i]/a;
+ printf("the avg is: %d",avg);
+ return 0;
+ }
+ 
