e4ebd20db3e399b7b89fed709cd1f9f74ba695c9
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-03-14 16:30:02 +0530

    Create power yes.c

---
+++ power yes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int first,second;
+    int result=1;
+    int i = 0;
+    printf("Enter a number : ");
+    scanf("%d",&second);
+    printf("Power : ");
+    scanf("%d",&first);
+    for(i = 0;i<first;i++)
+    {
+        result = result * second;
+    }
+    printf("%d to the first of %d is = %d\n",second,first,result);
+    printf("yes");
+    return 0;
+}
