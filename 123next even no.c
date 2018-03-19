6689262040a4a40c0e0ce2c8289b87ad5fd06622
savitharavi <31505899+savitharavi@users.noreply.github.com>
2018-03-19 15:45:33 +0530

    Update 123next even no.c

--- 123next even no.c
+++ 123next even no.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,c;
+    int a,b,c,i;
     printf("Enter the values:");
     scanf("%d",&a);
-    b=++a;
+    b=--a;
     if(b%2==0)
     {
-    printf("the next even num is:%d",b);
-    }else
+    printf("the previous even num is:%d",b);
+    }
+    else
     {
-        c=++b;
-        printf("the next even no is:%d",c);
+        c=--b;
+        printf("the previous even no is:%d",c);
     }
     return 0;
 }
